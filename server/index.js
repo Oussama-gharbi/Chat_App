e43425a289bbbd3b19f6038db9013cc15cc6e406
Oussama-gharbi <124163293+Oussama-gharbi@users.noreply.github.com>
2023-05-23 10:24:19 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://10.165.147.178:3000",
     credentials: true,
   },
 });
